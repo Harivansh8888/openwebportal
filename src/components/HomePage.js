3df90d81ccd52587babeef37660f33ff0d0cf0ee
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-05-18 17:01:07 +0530

    NavBar + Footer

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import ButtonAppBar from './NavBar'
 import Footer from './Footer'
 
 export default function HomePage(){
     return(
         <div>
+            <ButtonAppBar/>
             <Footer/>
         </div>
     );
