862216edc78638c89be50e2b815020b8845b935b
Harivansh Mathur <mathurharivansh811@gmail.com>
2020-05-24 20:29:02 +0530

    Added Banner

---
+++ src/components/Banner.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+
+
+export default function Banner() {
+        return (
+            <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      
+      height={550}
+      src="https://source.unsplash.com/random?text=First slide&bg=373940"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      
+      height={550}
+      src="https://source.unsplash.com/random?text=Second slide&bg=282c34"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      
+      height={550}
+      src="https://source.unsplash.com/random?text=Third slide&bg=20232a"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+        )
+}
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,12 +4,14 @@ import Gallery from './Gallery'
 import Footer from './Footer'
 import About from './About'
 import Goals from './Goals'
+import Banner from './Banner'
 
 export default function HomePage(){
     return(
         <div>
             
             <ButtonAppBar/>
+            <Banner />
             <About />
             <Goals />
             <Gallery/>
